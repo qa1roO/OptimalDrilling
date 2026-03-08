e565fc58e533cb2f16147bbfe0fe048f3e83481f
Alex <qa1roffmv@gmail.com>
2026-03-08 21:14:53 +0300

    deleted mysor

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+DATASETS_DIR = PROJECT_ROOT / "datasets"
+IMAGES_DIR = PROJECT_ROOT / "images"
+NOTEBOOKS_DIR = PROJECT_ROOT / "notebooks"
